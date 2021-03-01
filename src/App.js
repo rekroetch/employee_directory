1f4ef26a6c77fe480608a9bbcec3f6e31f2a5684
Rachel Kroetch <rekroetch@ucdavis.edu>
2021-03-01 12:01:56 -0800

    split table head and body, map over employee list

--- src/App.js
+++ src/App.js
@@ -1,20 +1,59 @@
 // import './App.css';
+import React, { Component } from "react";
 import Header from './components/Header'
-import Table from './components/Table'
+import TableHead from './components/TableHead'
+import TableBody from './components/TableBody'
 import employees from "./employees.json";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Table 
-        name={employees[0].name}
-        role={employees[0].role}
-        salary={employees[0].salary}
-        id={employees[0].id}
-      />
-    </div>
-  );
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//       <Table 
+//         name={employees[0].name}
+//         role={employees[0].role}
+//         salary={employees[0].salary}
+//         id={employees[0].id}
+//       />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+class App extends Component {
+  // Setting this.state.employees to the employees json array
+  state = {
+    employees
+  };
+
+  removeEmployee = id => {
+    // Filter this.state.employees for employees with an id not equal to the id being removed
+    const employees = this.state.employees.filter(employee => employee.id !== id);
+    // Set this.state.employees equal to the new employees array
+    this.setState({ employees });
+  };
+
+  // Map over this.state.employees and render a Table component for each employee object
+  render() {
+    return (
+      <div>
+        <Header>employees List</Header>
+        <TableHead />
+        {this.state.employees.map(employee => (
+          <TableBody
+            // removeemployee={this.removeemployee}
+            id={employee.id}
+            key={employee.id}
+            name={employee.name}
+            // image={employee.image}
+            role={employee.role}
+            salary={employee.salary}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
