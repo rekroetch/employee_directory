be3263c8a58076f16eae0e0598e715bcdd50578d
Rachel Kroetch <rekroetch@ucdavis.edu>
2021-03-01 11:39:39 -0800

    Header with title

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import "../styles/Header.css";
+
+function Header() {
+    return (
+      <header className="header">
+        <h1>Employee Tracker</h1>
+      </header>
+    );
+}
+  
+export default Header;
