6f2af6df830de14c1510d9dbf7bf85c557069898
Rachel Kroetch <rekroetch@ucdavis.edu>
2021-03-03 09:48:33 -0800

    functions added for form input and onclick

--- src/App.js
+++ src/App.js
@@ -1,56 +1,63 @@
-// import './App.css';
+import './App.css';
 import React, { Component } from "react";
 import Header from './components/Header'
 import TableHead from './components/TableHead'
 import TableBody from './components/TableBody'
+import Search from './components/Search'
 import employees from "./employees.json";
 
-// function App() {
-//   return (
-//     <div>
-//       <Header />
-//       <Table 
-//         name={employees[0].name}
-//         role={employees[0].role}
-//         salary={employees[0].salary}
-//         id={employees[0].id}
-//       />
-//     </div>
-//   );
-// }
-
-// export default App;
 
 class App extends Component {
   // Setting this.state.employees to the employees json array
   state = {
-    employees
+    employees,
+    search: "" 
   };
 
-  removeEmployee = id => {
-    // Filter this.state.employees for employees with an id not equal to the id being removed
-    const employees = this.state.employees.filter(employee => employee.id !== id);
+  handleInputChange = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  formSubmit = (e) => {
+    e.preventDefault()
+    // Filter this.state.employees for employees with a role not equal to the role searched
+    const employees = this.state.employees.filter(employee => employee.role === this.state.search);
     // Set this.state.employees equal to the new employees array
     this.setState({ employees });
+  }
+
+  reset = () => {
+    // refresh page
+    window.location.reload(false)
   };
 
   // Map over this.state.employees and render a Table component for each employee object
   render() {
     return (
       <div>
-        <Header>employees List</Header>
-        <TableHead />
-        {this.state.employees.map(employee => (
-          <TableBody
-            // removeemployee={this.removeemployee}
-            id={employee.id}
-            key={employee.id}
-            name={employee.name}
-            // image={employee.image}
-            role={employee.role}
-            salary={employee.salary}
-          />
-        ))}
+        <Header />
+        <Search 
+        handleInputChange={this.handleInputChange} 
+        formSubmit={this.formSubmit}/>
+        <table className="table container">
+          <TableHead />
+          {this.state.employees.map(employee => (
+            <TableBody
+              id={employee.id}
+              // not sure what this key does
+              key={employee.id}
+              name={employee.name}
+              role={employee.role}
+              phone={employee.phone}
+              email={employee.email}
+              dob={employee.dob}
+            />
+            ))}
+        </table>
       </div>
     );
   }
