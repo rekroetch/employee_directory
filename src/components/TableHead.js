bdf6fb623430b890bf1ab7624ace9968b04baa97
Rachel Kroetch <rekroetch@ucdavis.edu>
2021-03-03 08:48:59 -0800

    columns added for email and DOB

--- src/components/TableHead.js
+++ src/components/TableHead.js
@@ -1,16 +1,18 @@
 function TableHead() {
     return (
-        <table className="table table-striped">
         <thead>
           <tr>
             <th scope="col">ID</th>
             <th scope="col">Name</th>
             <th scope="col">Role</th>
-            <th scope="col">Salary</th>
+            <th scope="col">Phone Number</th>
+            <th scope="col">Email</th>
+            <th scope="col">Birthday</th>
           </tr>
         </thead>
+      //   <table className="table table-striped">
         
-      </table>
+      // </table>
     );
   }
   
