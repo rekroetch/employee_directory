6899b7b691536a298151105addfe0b96cb52c920
Rachel Kroetch <rekroetch@ucdavis.edu>
2021-03-03 08:47:02 -0800

    changed title to Employee Directory

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import "../styles/Header.css";
 function Header() {
     return (
       <header className="header">
-        <h1>Employee Tracker</h1>
+        <h1>Employee Directory</h1>
       </header>
     );
 }
