cd4681b5840bbed4f96094454005f26e43e65d45
Rachel Kroetch <rekroetch@ucdavis.edu>
2021-03-03 18:18:52 -0800

    removed commented out code

--- src/components/TableBody.js
+++ src/components/TableBody.js
@@ -1,17 +1,15 @@
 function TableBody(props) {
     return (
-            <tbody>
-                <tr>
-                    <td>{props.id}</td>
-                    <td>{props.name}</td>
-                    <td>{props.role}</td>
-                    <td>{props.phone}</td>
-                    <td>{props.email}</td>
-                    <td>{props.dob}</td>
-                </tr>
-            </tbody>
-        // <table className="table table-striped">
-        // </table>
+        <tbody>
+            <tr>
+                <td>{props.id}</td>
+                <td>{props.name}</td>
+                <td>{props.role}</td>
+                <td>{props.phone}</td>
+                <td>{props.email}</td>
+                <td>{props.dob}</td>
+            </tr>
+        </tbody>
     );
 }
   
